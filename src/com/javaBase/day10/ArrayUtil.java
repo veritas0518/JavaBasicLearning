de3365b9a6a5717d7f1efa0d982b0c2c377e9e44
xiaoXue <1450985307@qq.com>
2021-11-25 09:23:44 +0800

    ArrayUtil.java

---
+++ src/com/javaBase/day10/ArrayUtil.java
@@ -0,0 +1,97 @@
+package com.javaBase.day10;
+
+/**
+ * @Descripton:
+ * @Author:薛天行 Email:1450985307@qq.com or github.com/veritas0518
+ * @Belong project:
+ * @Belong package:
+ * @Date:Create in 17:02 2021/6/28
+ */
+public class ArrayUtil {
+    //求数组的最大值
+    public int getMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    //求数组的最小值
+    public int getMin(int[] arr) {
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    //求数组的总和
+    public int getSum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    //求数组的平均值
+    public double getAvg(int[] arr) {
+        int sum = getSum(arr);
+        double ave = (double) sum / arr.length;
+        return ave;
+    }
+
+    //反转数组
+    public void reverse(int[] arr) {
+        for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    //复制数组
+    public int[] copy(int[] arr) {
+        int[] arr1 = new int[arr.length];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = arr[i];
+        }
+        return arr1;
+    }
+
+    //数组排序
+    public void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    //遍历
+    public void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    //查找指定元素
+    public int getIndex(int[] arr, int index) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (index == arr[i]) {
+                return i;
+            }
+        }
+        return -1;  //返回一个负数表示没有找到
+    }
+}
