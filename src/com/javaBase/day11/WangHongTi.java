2b6fcb61318f471a6f3b7a10584424f60a5ca686
xiaoXue <1450985307@qq.com>
2021-11-26 15:39:33 +0800

    WangHongTi.java

---
+++ src/com/javaBase/day11/WangHongTi.java
@@ -0,0 +1,26 @@
+package com.javaBase.day11;
+
+/**
+ * @Descripton:
+ * @Author:薛天行 Email:1450985307@qq.com or github.com/veritas0518
+ * @Belong project:
+ * @Belong package:
+ * @Date:Create in 19:16 2021/6/29
+ */
+public class WangHongTi {
+    public static void main(String[] args) {
+        int m=10;
+        int n=20;
+    method(m,n);
+        System.out.println("m="+m);
+        System.out.println("n="+n);
+    }
+    public static void method(int m,int n){
+m*=10;
+n*=20;
+        System.out.println("m="+m);
+        System.out.println("n="+n);
+        System.exit(0);
+    }
+}
+
