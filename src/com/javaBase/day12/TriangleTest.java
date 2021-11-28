1354274f81103a707c28afe480b599855ac7cada
xiaoXue <1450985307@qq.com>
2021-11-28 20:23:20 +0800

    TriangleTest.java

---
+++ src/com/javaBase/day12/TriangleTest.java
@@ -0,0 +1,17 @@
+package com.javaBase.day12;
+
+/**
+ * @Descripton:
+ * @Author:薛天行 Email:1450985307@qq.com or github.com/veritas0518
+ * @Belong project:
+ * @Belong package:
+ * @Date:Create in 19:25 2021/6/30
+ */
+public class TriangleTest {
+    public static void main(String[] args) {
+        Triangle t1 =new Triangle();
+        t1.setBase(2.0);
+        t1.setHeight(2.4);
+        System.out.println("base: "+t1.getBase()+",height: "+ t1.getHeight());
+    }
+}
