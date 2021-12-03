a24bb143e0de55560845f822acc2029956429c2f
xiaoXue <1450985307@qq.com>
2021-12-03 13:12:18 +0800

    Customer.java

---
+++ src/com/javaBase/day13/evening/Customer.java
@@ -0,0 +1,36 @@
+package com.javaBase.day13.evening;
+
+/**
+ * @Descripton:
+ * @Author:薛天行 Email:1450985307@qq.com or github.com/veritas0518
+ * @Belong project:
+ * @Belong package:
+ * @Date:Create in 18:32 2021/7/1
+ */
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private Account account;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account acct) {
+        this.account = acct;
+    }
+}
