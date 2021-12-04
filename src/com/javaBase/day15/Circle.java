d4e6517ed46ff98f77e60240fe9247195202c0c9
xiaoXue <1450985307@qq.com>
2021-12-04 17:34:02 +0800

    Circle.java

---
+++ src/com/javaBase/day15/Circle.java
@@ -0,0 +1,36 @@
+package com.javaBase.day15;
+
+/**
+ * @Descripton: 父类Circle
+ * @Author:薛天行 Email:1450985307@qq.com or github.com/veritas0518
+ * @Belong project:
+ * @Belong package:
+ * @Date:Create in 19:57 2021/7/3
+ */
+public class Circle {
+    private double radius;
+
+    public Circle() {
+        radius = 1.0;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double findArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
