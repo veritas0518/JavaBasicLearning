9dc1623c2449ecdce6874e39070f0bc9fbdeeb32
xiaoXue <1450985307@qq.com>
2021-12-05 09:38:34 +0800

    Creature.java

---
+++ src/com/javaBase/day15/Creature.java
@@ -0,0 +1,16 @@
+package com.javaBase.day15;
+
+/**
+ * @Descripton:
+ * @Author:薛天行 Email:1450985307@qq.com or github.com/veritas0518
+ * @Belong project:
+ * @Belong package:
+ * @Date:Create in 19:03 2021/7/3
+ */
+public class Creature {
+    Person p1 = new Person();
+
+    public void breathe() {
+        System.out.println("呼吸");
+    }
+}
