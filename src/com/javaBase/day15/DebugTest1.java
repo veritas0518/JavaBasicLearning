3d27db4a2e3b3c22b27973db671a61daea8d0bb1
xiaoXue <1450985307@qq.com>
2021-12-06 17:36:42 +0800

    DebugTest1.java

---
+++ src/com/javaBase/day15/DebugTest1.java
@@ -0,0 +1,18 @@
+package com.javaBase.day15;
+
+/**
+ * @Descripton:  Debug的使用2
+ * @Author:薛天行 Email:1450985307@qq.com or github.com/veritas0518
+ * @Belong project:
+ * @Belong package:
+ * @Date:Create in 20:19 2021/7/3
+ */
+public class DebugTest1 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        System.out.println(arr);
+
+        char[] arr1 = new char[]{'a', 'b', 'c'};
+        System.out.println(arr1);
+    }
+}
