0d73e23802e1ea4e94302cc3890f7b27c11ecac2
xiaoXue <1450985307@qq.com>
2021-12-12 12:03:28 +0800

    Person.java

---
+++ src/com/javaBase/day17/Person.java
@@ -0,0 +1,27 @@
+package com.javaBase.day17;
+
+/**
+ * @Descripton:
+ * @Author:薛天行 Email:1450985307@qq.com or github.com/veritas0518
+ * @Belong project:
+ * @Belong package:
+ * @Date:Create in 17:28 2021/7/5
+ */
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+    }
+
+    public Person() {
+
+    }
+
+    public void eat(){
+        System.out.println("吃饭");
+    }
+    public void walk(){
+        System.out.println("人，走路");
+    }
+}
