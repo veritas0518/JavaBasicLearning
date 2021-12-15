090fb86d9d29d2e3ea1c717440f04b9e614e8202
xiaoXue <1450985307@qq.com>
2021-12-15 11:52:11 +0800

    GeometricObject.java

---
+++ src/com/javaBase/day18/evening/GeometricObject.java
@@ -0,0 +1,41 @@
+package com.javaBase.day18.evening;
+
+/**
+ * @Descripton:
+ * @Author:薛天行 Email:1450985307@qq.com or github.com/veritas0518
+ * @Belong project:
+ * @Belong package:
+ * @Date:Create in 20:32 2021/7/6
+ */
+public class GeometricObject {
+    protected String color;
+    protected double weight;
+
+    protected GeometricObject() {
+        this.color = "white";
+        this.weight = 1.0;
+    }
+
+    protected GeometricObject(String color, double weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+
+}
