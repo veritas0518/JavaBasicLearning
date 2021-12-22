e726dfd32e36ec29182032f51a1d66b8aea1e1cd
xiaoXue <1450985307@qq.com>
2021-12-22 12:52:02 +0800

    CommonEmployee.java

---
+++ src/com/javaBase/day22/evening/CommonEmployee.java
@@ -0,0 +1,14 @@
+package com.javaBase.day22.evening;
+
+/**
+ * @Descripton:
+ * @Author:薛天行 Email:1450985307@qq.com or github.com/veritas0518
+ * @Belong project:
+ * @Belong package:
+ * @Date:Create in 19:57 2021/7/10
+ */
+public class CommonEmployee extends Employee{
+    public void work(){
+        System.out.println("员工在一线车间生产产品");
+    }
+}
