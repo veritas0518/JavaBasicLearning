8c5f4a881c74264b597e6ce9537a644d4df4bd0b
xiaoXue <1450985307@qq.com>
2021-12-26 10:59:33 +0800

    Employee.java

---
+++ src/com/javaBase/day23/Employee.java
@@ -0,0 +1,54 @@
+package com.javaBase.day23;
+
+/**
+ * @Descripton:
+ * @Author:薛天行 Email:1450985307@qq.com or github.com/veritas0518
+ * @Belong project:
+ * @Belong package:
+ * @Date:Create in 16:14 2021/7/11
+ */
+public abstract class Employee {
+    private String name;
+    private int number;
+    private MyDate birthday;
+
+    public abstract double earnings();
+
+    public Employee() {
+    }
+
+    public Employee(String name, int number, MyDate birthday) {
+        this.name = name;
+        this.number = number;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public MyDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(MyDate birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' + ", number=" + number + ", birthday=" + birthday.toDateString() ;
+    }
+}
