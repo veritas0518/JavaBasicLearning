a4062461ecc7392c5c9499914cf3432e0d10fc4d
xiaoXue <1450985307@qq.com>
2021-12-29 16:42:53 +0800

    Circle.java

---
+++ src/com/javaBase/Day24/Evening/Circle.java
@@ -0,0 +1,24 @@
+package com.javaBase.Day24.Evening;
+
+/**
+ * @Descripton:
+ * @Author:薛天行 Email:1450985307@qq.com or github.com/veritas0518
+ * @Belong project:
+ * @Belong package:
+ * @Date:Create in 20:19 2021/7/14
+ */
+public class Circle {
+    private Double radius;
+    public Circle(){
+
+    }
+    public Circle(Double radius){
+        this.radius =radius;
+    }
+    public void setRadius(Double radius){
+        this.radius=radius;
+    }
+    public Double getRadius(){
+        return radius;
+    }
+}
