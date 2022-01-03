89e59286d4a8dffc1145d0e072ca46fc2b706eb2
xiaoXue <1450985307@qq.com>
2022-01-03 16:50:08 +0800

    ComparableCircle.java

---
+++ src/com/javaBase/Day24/ComparableCircle.java
@@ -0,0 +1,29 @@
+package com.javaBase.Day24;
+
+public class ComparableCircle extends Circle implements CompareObject {
+
+    public ComparableCircle(double radius){
+        super(radius);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if(this ==o){
+            return 0;
+        }
+        if(o instanceof ComparableCircle){
+            ComparableCircle c =(ComparableCircle) o;
+//            if(this.getRadius()>c.getRadius()){
+//                return 1;
+//            }else if(this.getRadius()<c.getRadius()){
+//                return -1;
+//            }else{
+//                return 0;
+//            }
+
+            return this.getRadius().compareTo(c.getRadius());
+        }else{
+            throw new RuntimeException("传入的数据类型不匹配");
+        }
+    }
+}
