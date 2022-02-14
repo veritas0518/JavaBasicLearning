ad891c1fa91a4c72f275a11f8f9a9672f28dad72
xiaoXue <1450985307@qq.com>
2022-02-14 11:24:58 +0800

    MyAnnotation.java

---
+++ src/com/javaSenior/Day45/exercise1/MyAnnotation.java
@@ -0,0 +1,13 @@
+package com.javaSenior.Day45.exercise1;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+@Target({TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyAnnotation {
+    String[] value() default "hello";
+}
