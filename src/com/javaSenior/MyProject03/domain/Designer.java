9cb314319b175732360a3cc79bd90f8f0459b407
xiaoXue <1450985307@qq.com>
2022-02-24 09:18:17 +0800

    Designer.java

---
+++ src/com/javaSenior/MyProject03/domain/Designer.java
@@ -0,0 +1,29 @@
+package com.javaSenior.MyProject03.domain;
+
+public class Designer extends Programmer {
+    private double bonus;  //奖金
+
+    public Designer() {
+    }
+
+    public Designer(int id, String name, int age, double salary, Equipment equipment, double bonus) {
+        super(id, name, age, salary, equipment);
+        this.bonus = bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public double getBonus(){
+        return bonus;
+    }
+
+    @Override
+    public String toString() {
+            return getDetails() + "\t设计师\t"+getStatus()+"\t"+bonus+"\t\t"+getEquipment().getDescription();
+    }
+    public String getDetailsForTeam() {
+        return getTeamBaseDetails() + "\t设计师"+getBonus();
+    }
+}
